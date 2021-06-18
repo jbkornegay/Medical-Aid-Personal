4d98d25fc8c0d9131afd4d905c12120a344b76e9
jbkornegay <jbkornegay@yahoo.com>
2021-06-18 17:56:55 -0400

    created models

--- aidApp/models.py
+++ aidApp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Pharmacy(models.Model):
+    name = models.CharField(max_length = 100)
+    street_address = models.CharField(max_length = 100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+    phone_number = models.IntegerField()
+    days_open = models.CharField(max_length=100)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+    website = models.CharField(max_length=100)
+
+class Clinic(models.Model):
+    doctors = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    street_address = models.CharField(max_length = 100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+    phone_number = models.IntegerField()
+    days_open = models.CharField(max_length=100)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+    
+
+class Doctor(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50)
+    password = models.CharField(max_length=50)
+    street_address = models.CharField(max_length = 100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+    phone_number = models.IntegerField()
+    specialty = models.CharField(max_length=50)
+
+class Patient(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50)
+    password = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=25)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField(max_length=10)
+    appointments = models.DateTimeField
+    insurance = models.CharField(max_length=50)
