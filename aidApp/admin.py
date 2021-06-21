a9cc20a6968f4381bff65c1538638bd82f229db8
jbkornegay <jbkornegay@yahoo.com>
2021-06-21 10:59:08 -0400

    added models to admin.py

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pharmacy, Clinic, Doctor, Patient
 
 # Register your models here.
+
+admin.site.register(Pharmacy)
+admin.site.register(Clinic)
+admin.site.register(Doctor)
+admin.site.register(Patient)
